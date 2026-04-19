ac0fe05bbd9c17bfc7480f514ab91f6940450202
oi-dp <oi.dp@pm.me>
2026-04-20 02:17:51 +0700

    updating treesitter to use main

--- dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,27 +6,46 @@ return {
   cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
   opts_extend = { "ensure_installed" },
   opts = {
-    indent = { enable = true },
-    highlight = { enable = true },
     ensure_installed = {
       "bash",
+      "css",
+      "fish",
       "html",
       "javascript",
       "json",
       "lua",
-      "typescript",
       "python",
-      "fish",
-      "css",
-      "vim",
       "toml",
+      "typescript",
+      "vim",
     },
   },
   config = function(_, opts)
     local ts = require("nvim-treesitter")
-    ts.setup(opts)
 
-    -- Install parsers and wait for completion
-    ts.install(opts.ensure_installed):wait(300000)
+    if opts.ensure_installed and #opts.ensure_installed > 0 then
+      ts.install(opts.ensure_installed, { summary = false }):wait(300000)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true }),
+      callback = function(args)
+        local buf = args.buf
+        local ft = vim.bo[buf].filetype
+        local lang = vim.treesitter.language.get_lang(ft)
+
+        if not lang then
+          return
+        end
+
+        plang(vim.treesitter.start, buf, lang)
+
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+        -- Enable treesitter folding
+        vim.opt_local.foldmethod = "expr"
+        vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      end,
+    })
   end,
 }
